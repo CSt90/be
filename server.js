cca16fb489289a7a6b87d5e583e550f975323b10
Costas Stefanakis <cst22590@gmail.com>
2025-06-07 22:03:53 +0300

    full commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const cors = require('cors');
+const app = express()
+const port = 5000
+
+app.use(cors());
+
+const data = require('./houses.json')
+
+app.get('/api/houses', (req, res) => {
+  if (req.query.name == null) res.send(data);
+  else{
+    let houseName = req.query.name;
+    const house = data.filter(d => d.name.toLowerCase().includes(houseName))
+    res.send(house);
+  }
+})
+
+app.listen(port, () => {
+  console.log(`Express app listening on port ${port}`)
+})
